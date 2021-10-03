ac12ab14d7dc8947367a502f891af5c696ffd9e1
DanielSRV <daniel.rivas@uptc.edu.co>
2021-10-02 22:12:02 -0500

    third commit

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose  =  require('mongoose');
 
-mongoose.connect('mongodb://localhost/software')
+const dbUrl ='mongodb+srv://Daniel:3nxhbZiIRszL9MDV@cluster0.bt3xa.mongodb.net/software?retryWrites=true&w=majority'
+mongoose.connect(dbUrl,{useNewUrlParser:true})
 
 .then(db => console.log('DB is connected'))
 .catch(err => console.error(err));
