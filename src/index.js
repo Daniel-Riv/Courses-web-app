38cc5fb09667700dfe15c6181ad9363705ef5a12
DanielSRV <daniel.rivas@uptc.edu.co>
2021-10-05 17:03:41 -0500

    variables/about/pruebas

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const flash = require('connect-flash');
 const passport = require('passport');
 
 
-//inicialixacion
+//Inicia
 const app = express();
 require('./database');
 require('./config/passport');
@@ -48,7 +48,7 @@ app.use((req,res, next) => {
     res.locals.user= req.user || null; 
     next();
 })
-//routes
+//routes 
 app.use(require('./routes/index'));
 app.use(require('./routes/cursos'));
 app.use(require('./routes/users'));
